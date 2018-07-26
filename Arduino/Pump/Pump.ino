32366b39406fed91d10a29f2b3ca161e9f568418
Jason <jasonharriot@gmail.com>
2018-07-26 11:23:44 -0400

    Reverted to AccelStepper

--- Arduino/Pump/Pump.ino
+++ Arduino/Pump/Pump.ino
@@ -9,15 +9,14 @@
 
 #ifndef NOSERIAL
 //////////////////////
-/// Comment out #define DOSERIAL to remove the majority of spam serial prints
-#define DOSERIAL//////
+/// Comment out #define DEBUGSERIAL to remove the majority of spam serial prints
+#define DEBUGSERIAL//////
 //////////////////////
 #endif
 
-
 #define FLIP_DIRECTION true //Flip the stepper DIR pin
 
-//#include "AccelStepper.h"
+#include "AccelStepper.h"
 #include "Montiey_Util.h"
 #include <SPI.h>
 #include <SD.h>
@@ -41,10 +40,10 @@ const char * flagQB = "qb";
 
 char * dataLine = (char *) calloc(MAX_LINE_BYTES, 1);    //A single command line may contain 64 characters. Increase if more memory is availible.
 
-//AccelStepper s(1, STEP, DIR); //1 = "driver mode" (operate with pulse and direction pins)
-MicroTimer stepTimer(0);
+AccelStepper s(1, STEP, DIR); //1 = "driver mode" (operate with pulse and direction pins)
+//MicroTimer stepTimer(0);
 HandyTimer recalculationInterval(125); //How often to poll the stepper for new steps (NOT step speed) (HIGHER intervals seem to yield smoother results
-bool stepState;
+//bool stepState;
 int commandIndex = 0;   //Current line of text
 unsigned long offsetTime = 0;   //Time at which the routine was started (with button)
 
@@ -55,112 +54,113 @@ void(* reset)(void) = 0;    //I don't know how this works
 void getLine() {
     bool addNewline = false;
     if(!dataFile.available()) {
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.println("There is no remaining text in the file. Appending a possibly redundant newline.");
-        #endif
+#endif
         addNewline = true;
         endGame();
     }
-    
+
     char c = ' ';    //some irrelevant starting value
     byte i = 0; //counter
-    
+
     char x;
-    for(x = 0; x < MAX_LINE_BYTES; x++){
+    for(x = 0; x < MAX_LINE_BYTES; x++) {
         dataLine[x] = 0x00;
     }
-    if(addNewline){
+    if(addNewline) {
         dataLine[x+1] = '\n';
     }
 
     while (dataFile.available() && c != '\n' && i < MAX_LINE_BYTES) { //while there is data to be read
         c = dataFile.read(); //read in the data
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.print("New character loaded: ");
         Serial.println(c);
-        #endif
+#endif
         dataLine[i] = c;
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.print("Line so far: ");
         Serial.println(dataLine);
-        #endif
+#endif
         i++;
     }
-    #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
     Serial.print("[getLine()] Line content: ");
     Serial.println(dataLine);
-    #endif
+#endif
 }
 
 void endGame() {    //Stops everything until the button is pressed (i.e. after commands complete, or after an error to retry)
-    delay(1000);
-    #ifndef NOSERIAL
+//    stepTimer.disable = true;   //Will get re-enabled the first time setQ() is validly called
+#ifndef NOSERIAL
     Serial.println("END - Waiting for button");
-    #endif
+#endif
     while (!digitalRead(BUTTON));   //do a little debouncing
-    delay(1000);
+    delay(100);
     while (digitalRead(BUTTON));
     reset();
 }
 
 void doOpenLoopStuff() { //everything that needs to be done as often as possible
-    if(stepTimer.trigger()){
-        digitalWrite(STEP, stepState); //Bool since digitalRead is slow
-        stepState = !stepState;
-    }
+//    if(stepTimer.trigger()){
+//        digitalWrite(STEP, stepState); //Bool since digitalRead is slow
+//        stepState = !stepState;
+//    }
+    s.runSpeed();
 }
 
 void setQ(float q) {
-    if(q == 0){
-        stepTimer.disable = true;
-    } else{
-        stepTimer.disable = false;
-    }
+//    if(q == 0){
+//        stepTimer.disable = true;
+//    } else{
+//        stepTimer.disable = false;
+//    }
     float stepSpeed = (1.0 / (UL_PER_STEP / USTEP_RATE)) * q;
-    #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
     Serial.print("Set q: ");
     Serial.print(q);
     Serial.print(" Set speed: ");
     Serial.println(stepSpeed);
-    #endif
-    //s.setSpeed(stepSpeed);
-    stepTimer.updateInterval((1000.0 / stepSpeed) * 1000);
+#endif
+    s.setSpeed(stepSpeed);
+//    stepTimer.updateInterval((1000.0 / stepSpeed) * 1000);
 }
 
-float mapFloat(float x, float in_min, float in_max, float out_min, float out_max){
- return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
+float mapFloat(float x, float in_min, float in_max, float out_min, float out_max) {
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
 
 void setup() {
-    #ifndef NOSERIAL
+#ifndef NOSERIAL
     Serial.begin(115200);
-    #endif
-    #ifdef DOSERIAL
+#endif
+#ifdef DEBUGSERIAL
     Serial.println("======== Begin ========");
-    #endif
+#endif
     pinMode(LED, OUTPUT);
     pinMode(BUTTON, INPUT);
-    pinMode(STEP, OUTPUT);
-    pinMode(DIR, OUTPUT);
-    digitalWrite(DIR, FLIP_DIRECTION);    //change direction
+//    pinMode(STEP, OUTPUT);
+//    pinMode(DIR, OUTPUT);
+//    digitalWrite(DIR, FLIP_DIRECTION);    //change direction
     digitalWrite(BUTTON, HIGH); //internal pullup
 
     if (!SD.begin()) {
-        #ifndef NOSERIAL
+#ifndef NOSERIAL
         Serial.println("SD card not present");
-        #endif
+#endif
         endGame();
     }
     dataFile = SD.open("commands.txt");
     if (!dataFile) {
-        #ifndef NOSERIAL
+#ifndef NOSERIAL
         Serial.println("commands.txt not found");
-        #endif
+#endif
         endGame();
     }
 
-    //s.setMaxSpeed(1000000); //Something to do with acceleration, which we don't care about. Just use a very high number.
-    //s.setSpeed(0);  //Don't move anything do start with
+    s.setMaxSpeed(1000000); //Something to do with acceleration, which we don't care about. Just use a very high number.
+    s.setSpeed(0);  //Don't move anything do start with
 
     while (digitalRead(BUTTON)) {   //wait for the button to begin the routine
         //Nothing happens
@@ -169,9 +169,9 @@ void setup() {
 }
 
 void loop() {
-    #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
     Serial.println("================= Loop is now running. Getting next command: ");
-    #endif
+#endif
     getLine(); //get the next line
 
     if (strstr(dataLine, flagQA)) {   //if command is for gradient (if qa flag exists)
@@ -179,11 +179,11 @@ void loop() {
         float qa;
         float tb;
         float qb;
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.println("Begin: GRADIENT command parsing");
         Serial.println("Command: ");
         Serial.println(dataLine);
-        #endif
+#endif
 
         //Get data
         ta = atof(strstr(dataLine, flagTA) + 2);    //get buff @ the index of the flag buff, then parse it and return a float.
@@ -191,18 +191,18 @@ void loop() {
         tb = atof(strstr(dataLine, flagTB) + 2);
         qb = atof(strstr(dataLine, flagQB) + 2);
 
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.print("Command index: ");
         Serial.println(commandIndex);
         Serial.println(ta);
         Serial.println(qa);
         Serial.println(tb);
         Serial.println(qb);
-        #endif
+#endif
         if (ta == 0 && commandIndex != 0) { //Cheap way of figuring out if String.toFloat failed
-            #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
             Serial.println("This command has stopped the routine.");
-            #endif
+#endif
             endGame();
         }
 
@@ -227,31 +227,31 @@ void loop() {
             doOpenLoopStuff();  //While we wait, do all the things.
         }
     } else {
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.println("Begin: NORMAL command parsing");
         Serial.print("Command: ");
         Serial.println(dataLine);
-        #endif
+#endif
         float t;
         float q;
 
         //Get data
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.print("Data: ");
         Serial.println(strchr(dataLine, flagT) + 1);
-        #endif
+#endif
         t = atof(strchr(dataLine, flagT) + 1);
         q = atof(strchr(dataLine, flagQ) + 1);
-        #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
         Serial.print("Command index: ");
         Serial.println(commandIndex);
         Serial.println(t);
         Serial.println(q);
-        #endif
+#endif
         if (t == 0 && commandIndex != 0) { //Cheap way of figuring out if String.toFloat failed
-            #ifdef DOSERIAL
+#ifdef DEBUGSERIAL
             Serial.println("This command has stopped the routine.");
-            #endif
+#endif
             endGame();
         }
 
